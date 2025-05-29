f6d6bb0abb4676cd28dede2277d8d63e8d267ca5
Ajay Bandla <ajyban@gmail.com>
2025-05-29 08:30:08 +0000

    Add Login html

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,5 +1,29 @@
+import './Login.css';
+
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <div className="login-form d-flex align-items-center">      
+      <div className="container text-center">
+        <form>
+          <div class="row">
+            <div class="col-md-6 offset-md-3">
+              <div class="row mb-3">
+                <label htmlFor="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="email" />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label htmlFor="password" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="password" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    </div>
   )
 }
