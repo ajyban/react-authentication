b9177eb56ee7eeaf489700b2ca7c9440bb9af92d
Ajay Bandla <ajyban@gmail.com>
2025-05-29 07:39:14 +0000

    Add front end base app

---
+++ front-end/src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from 'react-router';
+
+export default function Layout() {
+  return (
+    <>
+      <nav className="navbar bg-black text-white">
+        <div className="container-fluid">
+          <Link to="/" className="nav-link text-white fw-bold">React Authentication App</Link>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  )
+}
